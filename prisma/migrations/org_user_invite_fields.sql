745d6a4dca7c427578f48c0e06de29f9c96718c0
John Steele <124938266+Gitwit22@users.noreply.github.com>
2026-04-12 13:04:29 -0500

    fix auth login by reverting invitedById schema change

--- prisma/migrations/org_user_invite_fields.sql
+++
@@ -1,4 +0,0 @@
--- Migration: org_user_invite_fields
--- Adds invitedById to User for audit trail of who created an org user account.
-
-ALTER TABLE "User" ADD COLUMN IF NOT EXISTS "invitedById" TEXT;
