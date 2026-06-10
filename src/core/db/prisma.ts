170902abe34c403fad07fa36ccdfa0e020bdf621
Gitwit22 <itstheplugllc@gmail.com>
2026-06-10 12:38:09 -0400

    eventure add update

--- src/core/db/prisma.ts
+++ src/core/db/prisma.ts
@@ -1,3 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+type EventurePrismaClient = PrismaClient & {
+	eventureInvite: Prisma.EventureInviteDelegate<any, any>;
+	eventurePersonnel: Prisma.EventurePersonnelDelegate<any, any>;
+};
+
+export const prisma = new PrismaClient() as EventurePrismaClient;
